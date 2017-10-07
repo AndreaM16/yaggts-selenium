26da78d99cbd0af68401e05ec4bf61a074b3f4c5
andream16 <andrea.medda.uni@gmail.com>
2017-10-07 15:24:50 +0200

    Accepts args

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 from time import sleep
 
-TRENDS_URL = 'https://trends.google.it'
-QUERY = '/trends/explore?date=today%205-y&geo=GB&q='
+TRENDS_URL = 'https://trends.google.it/trends/explore?'
+QUERY = 'date=2015-01-01%202017-10-07&geo=GB&q='
 
 def findAndClickByCssSelector(driver, classes):
     selector = '.' + '.'.join(classes)    
@@ -22,10 +22,10 @@ def findAndClickByCssSelector(driver, classes):
 profile = webdriver.FirefoxProfile()
 profile.set_preference('browser.download.folderList', 2)
 profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', '/home/andream16/Documents/devStuff/yaggts-selenium/')
+profile.set_preference('browser.download.dir', '/home/andream16/go/src/github.com/andream16/yaggts')
 profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 driver = webdriver.Firefox(profile)
-driver.get("https://trends.google.it/trends/explore?date=today%205-y&geo=GB&q=samsung")
+driver.get(TRENDS_URL + QUERY + sys.argv[0])
 sleep(5)
 findAndClickByCssSelector(driver, ['widget-actions-menu', 'ic_googleplus_reshare'])
 findAndClickByCssSelector(driver, ['widget-actions-item-icon', 'csv-image', 'flip-rtl'])
