02dda39debf00d4cac62385a3592a6ad15eac901
Alessio <alex_pili@hotmail.it>
2017-11-26 18:44:12 +0100

    fix for new trends page

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # yaggts-selenium
 Python Selenium Helper for Yaggts
+Steps:
+1. Install python
+2. Install selenium module with sudo install pip selenium
+3. Download last geckodriver from https://github.com/mozilla/geckodriver/releases
+4. Extract it and put it in /usr/bin
+5. Run with python main.py
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 import itertools
-
+import os
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
@@ -11,24 +11,25 @@ TRENDS_URL = 'https://trends.google.it/trends/explore?'
 QUERY = 'date=2015-01-01%202017-10-07&geo=GB&q='
 
 def findAndClickByCssSelector(driver, classes):
-    selector = '.' + '.'.join(classes)    
+    selector = '.'.join(classes)    
     try:
         btn = driver.find_element_by_css_selector(selector)
     except NoSuchElementException:
         return
     btn.click()
     return
-
+filepath = os.path.join('/home/pippo/go/src/github.com/andream16/yaggts', 'multiTimeline.csv')
+if os.path.exists(filepath):
+    os.remove(filepath)
 profile = webdriver.FirefoxProfile()
 profile.set_preference('browser.download.folderList', 2)
 profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', '/home/andream16/go/src/github.com/andream16/yaggts')
+profile.set_preference('browser.download.dir', '/home/pippo/go/src/github.com/andream16/yaggts')
 profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 driver = webdriver.Firefox(profile)
 driver.get(TRENDS_URL + QUERY + sys.argv[1])
 sleep(5)
-findAndClickByCssSelector(driver, ['widget-actions-menu', 'ic_googleplus_reshare'])
-findAndClickByCssSelector(driver, ['widget-actions-item-icon', 'csv-image', 'flip-rtl'])
+findAndClickByCssSelector(driver, 'widget-actions-item export')
 driver.close()
 
 quit()
